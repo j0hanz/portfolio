b302ca18bf205c637455a9a748bf369ed877e308
Linus Johansson <golfarenlj@gmail.com>
2025-02-03 15:36:25 +0000

    Adjust initial scale in MotionWrapper for improved animation effect

--- src/components/Motions.tsx
+++ src/components/Motions.tsx
@@ -8,7 +8,7 @@ interface MotionWrapperProps {
 const MotionWrapper: FC<MotionWrapperProps> = ({ children }) => {
   return (
     <motion.div
-      initial={{ opacity: 0, scale: 0.7 }}
+      initial={{ opacity: 0, scale: 0.6 }}
       whileInView={{ opacity: 1, scale: 1 }}
       transition={{ duration: 0.8, ease: 'easeInOut' }}
     >
